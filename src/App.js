b3838a3f2fb2529caef3137094af2d997a988a1a
Gbot-Prod <gilbert.dolz24@gmail.com>
2026-03-09 02:44:41 +0800

    Tangina mo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function AppContent() {
   return (
     <div style={{ display: "flex" }}>
       {!hideSidebar && <Sidebar />}
-      <div style={{ flex: 1, padding: "60px" }}>
+      <div style={{ flex: 1, padding: "30px" }}>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
